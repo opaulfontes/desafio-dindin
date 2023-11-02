bf97cb40bc4b98e7030d3a698f97691cf5505b68
Paul Fontes <paulfontes1@gmail.com>
2023-11-02 13:49:43 -0300

    Endpoint listar categorias criado

--- src/rotas.js
+++ src/rotas.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { cadastrarUsuario, obterPerfilUsuario, atualizarPerfilUsuario } = require('./controladores/usuario');
 const { login } = require('./controladores/login');
 const { filtroAutenticacao } = require('./intermediarios/autenticacao');
+const { listarCategorias } = require('./controladores/categoria');
 
 const rotas = express()
 
@@ -12,5 +13,7 @@ rotas.use(filtroAutenticacao)
 
 rotas.get('/usuario', obterPerfilUsuario);
 rotas.put('/usuario', atualizarPerfilUsuario);
+
+rotas.get('/categoria', listarCategorias);
 
 module.exports = rotas
