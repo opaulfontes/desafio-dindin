936664d05ba82a5aacc79ff9246624289b64bf86
Paul Fontes <paulfontes1@gmail.com>
2023-11-02 15:39:02 -0300

    Endpoint de atualização de transações criado com sucesso.

--- src/controladores/transacao.js
+++ src/controladores/transacao.js
@@ -77,8 +77,60 @@ const cadastrarTransacao = async (req, res) => {
     }
 }
 
+const atualizarTransacao = async (req, res) => {
+    const { usuario } = req;
+    const { id } = req.params;
+    const { descricao, valor, data, categoria_id, tipo} = req.body;
+
+    if (!descricao) {
+        return res.status(400).json({ mensagem: 'A descrição é obrigatória!'})
+    }
+    if (!valor) {
+        return res.status(400).json({ mensagem: 'O valor é obrigatório'})
+    }
+    if (!data) {
+        return res.status(400).json({ mensagem: 'A data é obrigatória!'})
+    }
+    if (!categoria_id) {
+        return res.status(400).json({ mensagem: 'Este campo é obrigatório!'})
+    }
+    if (!tipo) {
+        return res.status(400).json({ mensagem: 'O tipo é obrigatório!'})
+    }
+    if (tipo !== 'entrada' && tipo !== 'saida') {
+        return res.status(400).json({ mensagem: 'O tipo precisa ser: "entrada" ou "saída".'})
+    }
+
+    try {
+        const transacao = await query('select * from transacoes where usuario_id = $1 and id = $2', [usuario.id, id]);
+
+        if (transacao.rowCount <= 0) {
+            return res.status(404).json({ mensagem: 'A transação não existe' });
+        }
+
+        const categoria = await query('select * from categorias where id = $1', [categoria_id])
+
+        if (categoria.rowCount <= 0) {
+            return res.status(404).json({ mensagem: 'A categoria não existe!'});
+        }
+
+        const queryAtualizacao = 'update transacoes set descricao = $1, valor = $2, data = $3, categoria_id = $4, tipo = $5 where id = $6';
+        const paramAtualizacao = [descricao, valor, data, categoria_id, tipo, id];
+        const transacaoAtualizada = await query(queryAtualizacao, paramAtualizacao);
+    
+        if (transacaoAtualizada.rowCount <= 0) {
+            return res.status(500).json({ mensagem: `Erro interno: ${error.message}`});
+        }
+
+        return res.status(204).send();
+    } catch (error) {
+        return res.status(500).json({ mensagem: `Erro interno: ${error.message}`});
+    }
+}
+
 module.exports = {
     listarTransacoes,
     detalharTransacao,
-    cadastrarTransacao
+    cadastrarTransacao,
+    atualizarTransacao
 }
